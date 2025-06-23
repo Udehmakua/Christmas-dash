6b0d42a559541863a3d2ca4be3794ec3a0fe11a0
Udehmakua <94395143+Udehmakua@users.noreply.github.com>
2025-06-23 15:55:13 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const startDate = new Date("2025-06-23");
-const endDate = new Date("2025-07-2");
+const endDate = new Date("2025-07-02");
 
 function App() {
   const [winnersData, setWinnersData] = useState({});
